c93a552a8ee692930dbb28449a6730f73a33cc38
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-05-17 12:15:04 -0300

    fipe-tabela

--- script.js
+++ script.js
@@ -1,98 +1,109 @@
-const tableHead =  `<tr><th>Marca</th><th>Modelo</th><th>Ano</th><th>Preço</th></tr>` 
+const tableHead = `<tr><th>Marca</th><th>Modelo</th><th>Ano</th><th>Preço</th></tr>`;
 
 function createRow(obj){
-  let td1 = document.createElement("td")
-  let td2 = document.createElement("td")
-  let td3 = document.createElement("td")
-  let td4 = document.createElement("td")
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
 
-  td1.innerText = obj.marca
-  td2.innerText = obj.modelo
-  td3.innerText = obj.ano
-  td4.innerText = `R$${obj.preco},00`
+    td1.innerText = obj.marca;
+    td2.innerText = obj.modelo;
+    td3.innerText = obj.ano;
+    td4.innerText = `R$${obj.preco},00`;
 
-  let tr = document.createElement("tr")
-  tr.append(td1,td2,td3,td4)
+    let tr = document.createElement('tr');
+    tr.append(td1,td2,td3,td4)
 
-  return tr
+    return tr;
 }
 
 function renderRows(list){
-  let table = document.querySelector(".table")
-  table.innerHTML = ""
-  table.insertAdjacentElement("beforeend", tableHead)
+    let table = document.querySelector('.table');
+    table.innerHTML = '';
+    table.insertAdjacentHTML('beforeend',tableHead);
 
-  for (let i = 0; i < list.length; i++) {
-    let obj = list[i]
-    let element = createRow(obj)
-
-    table.appendChild(element) 
-  }
+    for(let i = 0; i<list.length ; i++ ){
+        let obj = list[i];
+        let element = createRow(obj);
+        
+        table.appendChild(element);
+    }
 }
 
-renderRows(tabela)
+renderRows(tabela);
 
-document.querySelector("#btn-search").addEventListener("click" , function(e){
-  e.preventDefault()
+document.querySelector('#btn-search').addEventListener('click', function(event){
+    event.preventDefault();
+    let searchInput = document.querySelector('#input-search');
+    let searchText = searchInput.value; 
 
-  let searchInput = querySelector("#input-search")
-  let searchText = searchInput.value
+    let filteredList = filterList(tabela, searchText);
 
-  let filteredList = filterList(tabela , searchText)
-
-  renderRows(filteredList)
-})
+    renderRows(filteredList)
+});
 
 function filterList(list, text){
-  let localList = []
-  text = text.toLowerCase()
-  
-  if(text.includes("marca: ")){
-    text = text.substring(6)
-    for(let i = 0; i< list.length; i++){
-        let obj = list[i]
-        if(obj.marca.toLowerCase().includes(text)){
-          localList.push(obj)
+    let localList = [];
+    text = text.toLowerCase();
+
+    if(text.includes('marca:')){
+        text = text.substring(6);
+        for(let i = 0 ; i< list.length ; i++){
+            let obj = list[i];
+            if(obj.marca.toLowerCase().includes(text)){
+                localList.push(obj);
+            }
+        }
+    }else if(text.includes('modelo:')){
+        text = text.substring(7);
+        for(let i = 0 ; i< list.length ; i++){
+            let obj = list[i];
+            if(obj.modelo.toLowerCase().includes(text)){
+                localList.push(obj);
+            }
+        }
+    }else{
+        for(let i = 0 ; i< list.length ; i++){
+            let obj = list[i];
+            if( obj.marca.toLowerCase().includes(text) || 
+                obj.modelo.toLowerCase().includes(text) || 
+                obj.ano.toString().includes(text) || 
+                obj.preco.toString().includes(text)
+            ){
+                localList.push(obj);
+            }
         }
     }
-  }else if(text.includes("modelo: ")){
-    text = text.substring(7)
-    for(let i = 0; i< list.length; i++){
-        let obj = list[i]
-        if(obj.marca.toLowerCase().includes(text)){
-          localList.push(obj)
-        }
-    }
-  }else{
-      for(let i = 0; i< list.length; i++){
-      let obj = list[i]
-      if( obj.marca.toLowerCase().includes(text) || 
-          obj.modelo.toLowerCase().includes(text) || 
-          obj.ano.toString().includes(text) || 
-          obj.preco.toString().includes(text) 
-        ){
-        localList.push(obj)
-      }
-    }
-  }
-  
-  return localList
+    return localList;
 }
 
+let lastSort = ''
 function sortData(e, type){
-  e.preventDefault()
+    e.preventDefault()
+    
+    let newList = [...tabela];
 
-  let newList = [...tabela]
-
-  newList.sort(function(a,b){
-    if(a.marca < b.marca){
-      return -1
-    }else if(a.marca > b.marca){
-      return 1
+    if(lastSort == type){
+        newList.sort(function(a, b) {
+            if (a[type] > b[type]) {
+              return -1;
+            }else if (a[type] < b[type]) {
+              return 1;
+            }
+            return 0;
+        });
+        lastSort = '';
     }else{
-      return 0
+        newList.sort(function(a, b) {
+            if (a[type] < b[type]) {
+              return -1;
+            }else if (a[type] > b[type]) {
+              return 1;
+            }
+            return 0;
+        });
+        lastSort = type;
     }
-  })
 
-  renderRows(newList)
+    renderRows(newList);
 }
